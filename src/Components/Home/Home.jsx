6172bf8af96b28b51eb7f06a2a015446f6a09891
Venu <venugopal@marensolutions.com>
2022-11-10 11:09:45 +0530

    home page ui

--- src/Components/Home/Home.jsx
+++ src/Components/Home/Home.jsx
@@ -1,11 +1,23 @@
-import React from 'react'
+import React from 'react';
+import video from '../../assets/video.mp4';
 import "./home.css";
 
 const Home = () => {
   return (
-    <div>
-      Home
-    </div>
+    <section className='home'>
+      <div className="overLay"></div>
+      <video src={video} muted autoPlay loop typeof='video/mp4'></video>
+      <div className="homeContent container">
+        <div className="textDiv">
+          <spam className="smallText">
+            Our Packages
+          </spam>
+          <h1 className="homeTitle">
+            Search your holiday
+          </h1>
+        </div>
+      </div>
+    </section>
   )
 }
 
